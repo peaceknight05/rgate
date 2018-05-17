06a8f3c99c70583627edd82c658f0f39f23a4bb1
peaceknight05 <38887622+peaceknight05@users.noreply.github.com>
2018-05-17 21:24:44 +0800

    Add files via upload
    Added license and the ruby script

---
+++ lib/rgates.rb
@@ -0,0 +1,65 @@
+class Rgates
+  	def and(one, two)
+		if one == true && two == true
+			return true
+		else
+			return false
+		end
+	end
+
+	def or(one, two)
+		if one == true
+			return true
+		elsif two == true
+			return true
+		elsif one == true && two == true
+			return true
+		else
+			return false
+		end
+	end
+
+	def not(one)
+		if one == true
+			return false
+		else
+			return true
+		end
+	end
+
+	def nand(one, two)
+		if one == true && two == true
+			return false
+		else
+			return true
+		end
+	end
+
+	def nor(one, two)
+		if one == false && two == false
+			return true
+		else
+			return false
+		end
+	end
+
+	def xor(one, two)
+		if one == true && two == false
+			return true
+		elsif one == false && two == true
+			return true
+		else
+			return false
+		end
+	end
+
+	def xnor(one, two)
+		if one == true && two == true
+			return true
+		elsif one == false && two == false
+			return true
+		else
+			return false
+		end
+	end
+end
